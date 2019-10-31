e57da5fd6ee1d36fc42472c95668d99353096f03
shakur35 <miiro.shakur@gmail.com>
2019-10-31 19:35:56 -0400

    working sound at click of the button

--- assets/script/breweryAPIScript.js
+++ assets/script/breweryAPIScript.js
@@ -1,10 +1,25 @@
-$(document).ready(function () {
+$(document).ready(function() {
     state = "New Hampshire"
     url = `https://api.openbrewerydb.org/breweries?by_state=new_hampshire`;
     $.ajax({
         url: url,
         method: "GET"
-    }).done(function (response) {
+    }).done(function(response) {
         console.log(response);
+    });
+
+
+    var srx = $("#SearchButton")
+
+    srx.on("click", function() {
+
+        var sound = new Audio("assets/bottle_sound.wav");
+
+        sound.play();
+
+        console.log("working");
+
+
+
     });
 });
