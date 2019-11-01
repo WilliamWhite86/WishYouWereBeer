079857278b7a64d01dfc88d181a2802662b650a9
Clark Gesen <cgesen@gmail.com>
2019-10-31 21:05:55 -0400

    ipinfoAPI and google distance matrix API working

--- assets/script/googleAPIFunctionality.js
+++ assets/script/googleAPIFunctionality.js
@@ -1,23 +1,13 @@
 $(document).ready(function () {
-    var lat;
-    var lng;
     var destination = "231 Corporate Dr., Portsmouth, NH"
     apiKey = "AIzaSyAU_8wi9cIK0CHy40pS_wW2X6lGnc81pkg"
     url = `https://cors-anywhere.herokuapp.com/https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
-    $.ajax({
-        url: url,
-        method: "POST",
-        headers: { 'Access-Control-Allow-Origin': '*' },
-
-    }).done(function (response) {
-
-        console.log(response);
-        lat = response.location.lat;
-        lng = response.location.lng;
-
-        console.log(lat);
-        console.log(lng);
-        url2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${lng}&destinations=New+York+City,NY&key=${apiKey}`
+    $.get("https://ipinfo.io", function (response) {
+        console.log(response.ip, response.country);
+        city = response.city;
+        region = response.region;
+    
+        url2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${city},${region}&destinations=${destination}&key=${apiKey}`
 
         $.ajax({
             url: url2,
@@ -25,7 +15,5 @@ $(document).ready(function () {
         }).done(function (response) {
             console.log(response);
         });
-    });
-
-
+    }, "jsonp")
 });
