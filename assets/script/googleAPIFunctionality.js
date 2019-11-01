1f0ce8879384883716d178eee5d50b04a124dbd4
Clark Gesen <cgesen@gmail.com>
2019-10-31 20:40:13 -0400

    Added google distance matrix API

--- assets/script/googleAPIFunctionality.js
+++ assets/script/googleAPIFunctionality.js
@@ -5,14 +5,27 @@ $(document).ready(function () {
     apiKey = "AIzaSyAU_8wi9cIK0CHy40pS_wW2X6lGnc81pkg"
     url = `https://cors-anywhere.herokuapp.com/https://www.googleapis.com/geolocation/v1/geolocate?key=${apiKey}`;
     $.ajax({
-        url: url, 
-        method: "POST",  
-        headers: {'Access-Control-Allow-Origin': '*' },
-        
+        url: url,
+        method: "POST",
+        headers: { 'Access-Control-Allow-Origin': '*' },
+
     }).done(function (response) {
-        
+
         console.log(response);
         lat = response.location.lat;
         lng = response.location.lng;
+
+        console.log(lat);
+        console.log(lng);
+        url2 = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${lat},${lng}&destinations=New+York+City,NY&key=${apiKey}`
+
+        $.ajax({
+            url: url2,
+            method: "POST",
+        }).done(function (response) {
+            console.log(response);
+        });
     });
+
+
 });
