d476de423fea12a293016a981f75d574b54f6490
shakur35 <miiro.shakur@gmail.com>
2019-11-02 10:55:32 -0400

    Our app now display an image of 2 beer bottles for 3 secs

--- assets/script/breweryAPIScript.js
+++ assets/script/breweryAPIScript.js
@@ -9,9 +9,77 @@ $(document).ready(function() {
     });
 
 
-    var srx = $("#SearchButton")
+    var srx = $("#SearchButton");
+
+    var pic = $(".overlay");
+
+    function displayOverlay(img) {
+        var overlay = $("<div>").css({
+            "position": "fixed",
+            "top": 0,
+            "left": 0,
+            "width": "100%",
+            "height": "100%",
+            "background-color": "rgba(0,0,0,.5)",
+            "z-index": 10000,
+            "vertical-align": "middle",
+            "color": "#fff",
+            "font-size": "30px",
+            "font-weight": "bold",
+            "cursor": "wait",
+
+        }).appendTo("body");
+
+        var pic = $(img).css({
+
+            "position": "fixed",
+            "left": "25%",
+            "margin": "auto",
+            "z-index": 10001,
+
+            "width": "50%"
+
+
+
+
+        }).appendTo(overlay);
+
+
+        setTimeout(function() {
+
+            overlay.remove();
+
+        }, 3000);
+    };
+
+
+    // function removeOverlay() {
+
+    // }
+
+
+
+
+
+
+
+
+
 
     srx.on("click", function() {
+
+
+
+        displayOverlay('<img src="https://media.giphy.com/media/21I1WgRqKQaT8TRdmq/giphy.gif">');
+
+        // removeOverlay();
+
+
+
+        // clearTimeout(displayOverlay);
+
+
+
 
         var sound = new Audio("assets/bottle_sound.wav");
 
@@ -23,3 +91,6 @@ $(document).ready(function() {
 
     });
 });
+
+
+// giphyKey = "hoKvwwGpkod07r23TMg1Ksg56Kzt7RNH";
