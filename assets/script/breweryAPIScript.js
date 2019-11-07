af2eeeab386ca3a9be5f3f0f25eaa8a0de5276ec
William White <william.white86@gmail.com>
2019-11-07 18:30:48 -0500

    add sorting and printlist functions

--- assets/script/breweryAPIScript.js
+++ assets/script/breweryAPIScript.js
@@ -2,15 +2,12 @@
 $(document).ready(function () {
 
     var arr = JSON.parse(localStorage.getItem("myarea")) || [];
+    var placeObj = [];
 
     $("#SearchButton").on('click', function () {
         $("#searchField").empty();
         $("#names").empty();
         event.preventDefault();
-
-        var srx = $("#SearchButton");
-
-        var pic = $(".overlay");
 
         function displayOverlay(img) {
             var overlay = $("<div>").css({
@@ -29,7 +26,7 @@ $(document).ready(function () {
 
             }).appendTo("body");
 
-            var pic = $(img).css({
+            $(img).css({
 
                 "position": "fixed",
                 "left": "25%",
@@ -52,36 +49,27 @@ $(document).ready(function () {
         var sound = new Audio("assets/bottle_sound.wav");
         sound.play();
 
-        console.log("working");
         $("#searchField").empty();
         $("#names").empty();
         event.preventDefault();
 
         addPlace();
-
         var stateinput = $("#searchField").val().trim();
         url = `https://api.openbrewerydb.org/breweries?by_state=${stateinput}`;
         $.ajax({
             url: url,
             method: "GET"
         }).done(function (response) {
-            console.log(response);
 
             Object.values(response).forEach((value) => {
-                var place = $("<tr>");
-                var name = $("<td>");
-                var website = $("<td>");
-                console.log(value.name);
-                name.text(value.name);
-                website.text(value.website_url)
-                $("#names").append(place);
-                place.append(name, website);
+                var place = {};
+                place.name = value.name;
+                place.website = value.website_url;
+
 
                 var destination = value.street + value.city;
-                console.log(destination);
                 apiKey = "AIzaSyAU_8wi9cIK0CHy40pS_wW2X6lGnc81pkg"
                 $.get("https://ipinfo.io", function (response) {
-                    console.log(response.ip, response.country);
                     city = response.city;
                     region = response.region;
 
@@ -91,73 +79,60 @@ $(document).ready(function () {
                         url: url2,
                         method: "POST",
                     }).done(function (response) {
-
                         var distanceArray = [];
                         distanceArray.push(response.rows[0].elements[0].distance.text);
-                        console.log(distanceArray);
                         for (var i = 0; i < distanceArray.length; i++) {
-                            var distance = $("<td>");
-                            distance.text(distanceArray[i]);
-                            place.append(distance);
+                            place.distance = parseInt(distanceArray[i].split(" ")[0]);
+
                         }
 
-                        //place.append(destination);
-                        //place.append(distance);
+
+
                         $("#searchHistoryField").empty();
                     });
                 }, "jsonp")
-
+                placeObj.push(place);
 
             });
+            setTimeout(sortList, 3000);
+            function sortList() {
+                placeObj.sort(function (a, b) {
+                    return a.distance - b.distance;
+                });
+            };
+
+            setTimeout(printList, 3000)
+            function printList() {
+                Object.values(placeObj).forEach((value) => {
+                    var place = $("<tr>");
+                    var name = $("<td>");
+                    var website = $("<td>");
+                    var distance = $("<td>");
+                    name.text(value.name);
+                    website.text(value.website)
+                    distance.text(value.distance + " miles")
+                    place.append(name, website, distance);
+                    $("#names").append(place);
+                })
+            }
         });
 
         $("#searchHistoryField").on("change", function () {
-            var city = $(this).val();
-            console.log(city);
         });
 
         function addPlace() {
-
             var stateinput = $("#searchField").val().trim();
-
             arr.push(stateinput);
-
             localStorage.setItem("myarea", JSON.stringify(arr));
-
             renderMyPlaces();
         }
-
         function renderMyPlaces() {
-
             $("#searchHistoryField").empty();
-
             for (i = 0; i < arr.length; i++) {
-
                 var newEl = $("<option>");
-
-
                 newEl.text(arr[i])
-
                 newEl.prependTo("#searchHistoryField");
-
             }
-
-
-
-
-
-
-
-
-
-
-
-
         };
-
-
-
-
-
     });
 });
